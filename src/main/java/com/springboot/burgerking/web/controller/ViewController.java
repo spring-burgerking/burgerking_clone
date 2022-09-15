ffe4a74389a16313a2ca55512827f7f5db7dab6f
ITPS <ITPS@gmail.com>
2022-09-15 22:25:18 +0900

    0915 infoChange 오타

--- src/main/java/com/springboot/burgerking/web/controller/ViewController.java
+++ src/main/java/com/springboot/burgerking/web/controller/ViewController.java
@@ -101,7 +101,7 @@ public class ViewController {
 	
 	@GetMapping("/auth/info-change")
 	public String infochange() {
-		return "/burgerking/auth/infochange";
+		return "/burgerking/auth/infoChange";
 	}
 	
 	@GetMapping("/auth/findUserInfo")
